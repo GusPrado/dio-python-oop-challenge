a262b1d653f3d09b902a5e15b65bcbb9fccda9b1
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-02-01 21:58:19 -0300

    feat: class definition

--- main.py
+++ main.py
@@ -1,6 +1,122 @@
-#Sistema Bancário
-#
+from abc import ABC, abstractclassmethod, abstractproperty
+from datetime import datetime
+class Conta: 
+  def __init__(self, numero, cliente):
+     self._saldo = 0
+     self._numero = numero
+     self._agencia = "0001"
+     self._cliente = _cliente
+     self._historico = Historico()
+     
 
+  @classmethod 
+  def nova_conta(cls, cliente, numero):
+      # Factory devolve instancia de Conta
+      return cls(cliente, numero)
+  
+  @property
+  def saldo(self):
+    return self._saldo
+  
+  @property
+  def numero(self):
+    return self._numero
+  
+  @property
+  def agencia(self):
+    return self._agencia
+  
+  @property
+  def cliente(self):
+    return self._cliente
+  
+  @property
+  def historico(self):
+    return self._historico
+   
+  def sacar(self, valor):
+      pass
+   
+  def depositar(self, valor):
+      pass
+
+class ContaCorrente(Conta):
+  def __init__(self, numero, cliente, limite=500, limite_saques=3):
+      super().__init__(numero, cliente)
+      self.limite = limite
+      self.limite_saques = limite_saques
+      
+  def sacar(self, valor):
+      #precisa fazer validações adicionais
+      pass
+
+class Historico:  
+  def __init__(self):
+      self.transacoes = []
+  @property
+  def transacoes(self):
+    return self._transcoes
+      
+  def adicionar_transacao(self, transacao):
+      pass
+  
+class Transacao(ABC):
+   @property
+   @abstractclassmethod
+   def valor(self):
+      pass
+   
+   @abstractclassmethod
+   def registrar(self, conta):
+      pass
+   
+class Saque(Transacao):
+  def __init__(self, valor):
+      self._valor = valor
+
+  @property
+  def valor(self):
+    return self._valor
+  
+  def registrar(self, conta):
+    sucesso_transacao = conta.sacar(self.valor)
+    if sucesso_transacao:
+       conta.historico.adicionar_transacao(self)
+  
+class Deposito(Transacao):
+  def __init__(self, valor):
+      self._valor = valor
+
+  @property
+  def valor(self):
+    return self._valor
+  
+  def registrar(self, conta):
+    sucesso_transacao = conta.depositar(self.valor)
+    if sucesso_transacao:
+       conta.historico.adicionar_transacao(self)
+  
+class Cliente:
+  def __init__(self, endereco):
+    self.endereco = endereco
+    self.contas = []
+    
+  
+  def realizar_transacao(self, conta, transacao)
+     transacao.registrar(conta)
+  
+  def adicionar_conta(self, conta):
+     self.contas.append(conta)
+
+class PessoaFisica(Cliente):
+   def __init__(self, cpf, nome, data_nascimento, endereco):
+      super().__init__(endereco)
+      self.cpf = cpf
+      self.nome = nome
+      self.data_nascimento = data_nascimento
+
+
+#########################################################
 
 def sacar(*, saldo, valor, extrato, saque_diario, limite_saques):
    
